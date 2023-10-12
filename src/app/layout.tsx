d8eb85ff1ed2a665856363794399da2705124ce9
Ben Vogler <benvogler1@gmail.com>
2023-10-12 02:00:19 -0400

    Add title, audio toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import './globals.css'
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Major_Mono_Display } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const major = Major_Mono_Display({
+  weight: '400',
+  subsets: ['latin']
+})
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -16,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={major.className + ' lowercase text-shadow-sm shadow-black text-sky-600'}>{children}</body>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
 'use client';
 import styles from './page.module.css';
+import { SpeakerWaveIcon } from '@heroicons/react/24/solid';
+import { SpeakerXMarkIcon } from '@heroicons/react/24/solid';
 
 export default function Home() {
   return (
     <main>
+      <div className={styles.backgroundVignette}></div>
       <video className={styles.backgroundVideo} autoPlay loop muted>
         <source src="background.mp4" type="video/mp4"></source>
       </video>
+
+      <div className={styles.interface + ' p-4'}>
+        <div className="flex justify-between">
+          <div>
+            <h1 className="text-lg text-sky-400">Stargazer Station</h1>
+            <h2 className="text-base">Pleione 10 d, Inner Orion Spur</h2>
+          </div>
+          <div className="flex gap-2 align-middle">
+            <div className="text-sm" id="volumeLabel">Audio Isolation: Engaged</div>
+            <div className="cursor-pointer text-sky-800 hover:scale-110 hover:drop-shadow-sm">
+              <SpeakerWaveIcon id="volumeOnIcon" className="h-6 w-6 hidden" onClick={toggleMute}/>
+              <SpeakerXMarkIcon id="volumeOffIcon" className="h-6 w-6" onClick={toggleMute}/>
+            </div>
+          </div>
+        </div>
+      </div>
     </main>
   )
 }
 
 function toggleMute() {
-
+  let video: HTMLVideoElement = document.querySelector(`.${styles.backgroundVideo}`)!;
+  video.muted = !video.muted;
+  document.querySelector('#volumeOnIcon')?.classList.toggle('hidden');
+  document.querySelector('#volumeOffIcon')?.classList.toggle('hidden');
+  document.querySelector('#volumeLabel')!.innerHTML = `Audio Isolation: ${video.muted ? 'Engaged' : 'Disengaged'}`;
 }
